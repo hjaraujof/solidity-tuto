4bb3a0eebd79b907f0aae3c3af28da30ccc87c32
Harold Araujo <haroldj.araujof@gmail.com>
2018-04-25 00:34:47 -0300

    Tests from tuto ready

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,28 +1,49 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface, bytecode } = require('../compile');
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-    
-    drive() {
-        return 'vroom';
-    }
-}
+// Silence MaxListeners Warning
+require('events').EventEmitter.prototype._maxListeners = 100;
 
-let car;
-beforeEach( () => {
-    car = new Car();
-})
+// Initial variables
+let accounts;
+let inbox;
+let initialString = 'Hi there';
+let initialGas = '1000000';
 
-describe('Car', () => {
-    it('can park', () => {
-        assert(car.park(), 'stopped');
+// Initialize every variable needed for the tests!
+beforeEach(async () => {
+    // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    // Use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy( { data: bytecode, arguments: [initialString] } )
+        .send({ from: accounts[0], gas: initialGas });
+    inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
     });
-    it('can drive', () => {
-        assert(car.drive(),'vroom');
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.getMessage().call();
+        assert.equal(message,initialString);
     });
-})
+
+    it('message can be changed', async () => {
+        const message = 'Test message';
+        //remember we have to pay the gas for changes on the Blockchain
+        await inbox.methods.setMessage(message).send({ from: accounts[0] });
+        
+        const retrievedMsg = await inbox.methods.getMessage().call();
+        assert.equal(retrievedMsg,message);
+    });
+
+});
